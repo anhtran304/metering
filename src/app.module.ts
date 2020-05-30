82155702d32c5183b246ff7de9ef8ef29bf733b3
kentran304 <tranmr@gmail.com>
2020-05-30 18:17:23 +1000

    BE: Fixed static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { UsersModule } from './users/users.module';
 import { StationsModule } from './stations/stations.module';
 import { RolesModule } from './roles/roles.module';
 import { PublicModule } from './public/public.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, UsersModule, StationsModule, RolesModule, PublicModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'web/build'),
+    }),
+    AuthModule,
+    UsersModule,
+    StationsModule,
+    RolesModule,
+    PublicModule,
+  ],
   // controllers: [AppController],
   // providers: [AppService],
 })
